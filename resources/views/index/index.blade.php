b9b5e7e5760debeec788e83851852cf6fe46f376
Kevin <kefthegamer100@gmail.com>
2022-03-21 15:08:19 +0100

    Removed commented code

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -8,7 +8,6 @@
 
     <div id="fullpage">
         @include('index.sections.landing')
-        {{--        https://codepen.io/layalk/pen/mmXpog --}}
         @include('index.sections.preferences')
         @include('index.sections.about')
         @include('index.sections.portfolio')
@@ -16,21 +15,4 @@
         @include('index.sections.contact')
     </div>
 
-
-    {{--    <div class="container">--}}
-    {{--        <div class="row justify-content-center">--}}
-    {{--            <div class="col-md-8">--}}
-    {{--                <div class="card">--}}
-    {{--                    <div class="card-body">--}}
-    {{--                        @if (session('status'))--}}
-    {{--                            <div class="alert alert-success" role="alert">--}}
-    {{--                                {{ session('status') }}--}}
-    {{--                            </div>--}}
-    {{--                        @endif--}}
-    {{--                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})--}}
-    {{--                    </div>--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-    {{--        </div>--}}
-    {{--    </div>--}}
 @endsection
