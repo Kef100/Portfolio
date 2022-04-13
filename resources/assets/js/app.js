ba3b6e9bc8a2c555ca0ecb8456d772bde087b263
Kevin <kefthegamer100@gmail.com>
2022-04-13 20:16:05 +0200

    Working slider

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -86,8 +86,8 @@ bullets.each(function() {
 function showSlide(slide) {
     let slideElement = slides.get(slide - 1);
     slides.each(function() {
+        $(this).removeClass("fade-active-slide");
         $(this).removeClass("active-portfolio-slide");
-        $(this).find('img').fadeTo(500, 1);
     });
     $(slideElement).addClass("active-portfolio-slide");
 
@@ -120,8 +120,13 @@ function goToSlide(slide) {
     if (slide !== sliderIndex) {
         clearInterval(interval);
         i = 0;
-        showSlide(slide);
-        showBullet(slide - 1);
+        let currentElement = slides.get(sliderIndex - 1);
+        $(currentElement).removeClass("active-portfolio-slide");
+        $(currentElement).addClass("fade-active-slide");
+        setTimeout(function () {
+            showSlide(slide);
+            showBullet(slide - 1);
+        }, 1000);
         sliderIndex = slide;
     }
 }
@@ -129,6 +134,9 @@ function goToSlide(slide) {
 
 function startCounter(time, slide) {
     let bullet = bullets.get(slide - 1);
+    let slideElement = slides.get(slide - 1);
+    let isFading = false;
+
     interval = setInterval(function () {
         if (i == time) {
             clearInterval(interval);
@@ -144,6 +152,12 @@ function startCounter(time, slide) {
             }
             return;
         }
+
+        if(i / time * 100 > 85 && !isFading) {
+            isFading = true;
+            $(slideElement).removeClass("active-portfolio-slide");
+            $(slideElement).addClass("fade-active-slide");
+        }
         $(bullet).find("svg circle:nth-child(2)").css('stroke-dashoffset', dasharray - ((i + 1) * (dasharray / time)));
         i++;
     }, 1);
@@ -153,12 +167,22 @@ $("#nextSlide").click(function () {
     if (slides.length >= (sliderIndex + 1)) {
         clearInterval(interval);
         i = 0;
-        nextSlide(sliderIndex - 1);
+        let slideElement = slides.get(sliderIndex - 1);
+        $(slideElement).removeClass("active-portfolio-slide");
+        $(slideElement).addClass("fade-active-slide");
+        setTimeout(function () {
+            nextSlide(sliderIndex - 1);
+        }, 1000);
     }else{
         clearInterval(interval);
         i = 0;
         sliderIndex = 0;
-        nextSlide(0);
+        let slideElement = slides.get(slides.length - 1);
+        $(slideElement).removeClass("active-portfolio-slide");
+        $(slideElement).addClass("fade-active-slide");
+        setTimeout(function () {
+            nextSlide(0);
+        }, 1000);
     }
 });
 
@@ -166,11 +190,29 @@ $("#previousSlide").click(function () {
     if ((sliderIndex - 1) >= 1) {
         clearInterval(interval);
         i = 0;
-        previousSlide(sliderIndex);
+        let slideElement = slides.get(sliderIndex);
+        $(slideElement).removeClass("active-portfolio-slide");
+        $(slideElement).addClass("fade-active-slide");
+        setTimeout(function () {
+            previousSlide(sliderIndex);
+        }, 1000);
     } else {
         clearInterval(interval);
         sliderIndex = slides.length + 1;
         i = 0;
-        previousSlide(slides.length);
+        let slideElement = slides.get(slides.length - 1);
+        $(slideElement).removeClass("active-portfolio-slide");
+        $(slideElement).addClass("fade-active-slide");
+        setTimeout(function () {
+            previousSlide(slides.length);
+        }, 1000);
     }
 });
+
+// $("#pause").click(function () {
+//     clearInterval(interval);
+// });
+//
+// $("#play").click(function () {
+//     showSlide(sliderIndex);
+// });
